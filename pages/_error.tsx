4e071ef89ca7afecd456011b76e8055f058e06c9
dk017 <dhineshkumar.stoic@gmail.com>
2024-10-08 14:48:35 +0530

    resolving error page

---
+++ pages/_error.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+
+import React from 'react';
+import { NextPageContext } from 'next';
+
+export const runtime = 'experimental-edge';
+
+interface ErrorProps {
+  statusCode: number;
+}
+
+function Error({ statusCode }: ErrorProps) {
+  return (
+    <p>
+      {statusCode
+        ? `An error ${statusCode} occurred on server`
+        : 'An error occurred on client'}
+    </p>
+  );
+}
+
+Error.getInitialProps = ({ res, err }: NextPageContext): ErrorProps => {
+    const statusCode = res?.statusCode ?? err?.statusCode ?? 404;
+    return { statusCode };
+};
+
+
+export default Error;
