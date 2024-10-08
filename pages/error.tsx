cd9b03d13dd65b847ba95f0e1a9aa68f8997cbaf
dk017 <dhineshkumar.stoic@gmail.com>
2024-10-08 10:30:42 +0530

    added error file to pages folder

---
+++ pages/error.tsx
@@ -0,0 +1,13 @@
+'use client'
+export const runtime = 'edge';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1>Something went wrong!</h1>
+            <p>Please try again later.</p>
+        </div>
+    );
+};
+
+export default ErrorPage;
