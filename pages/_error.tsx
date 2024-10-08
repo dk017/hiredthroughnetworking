7f2063d2e6d7f01a138051ae56790b2fbf756c73
dk017 <dhineshkumar.stoic@gmail.com>
2024-10-08 16:44:18 +0530

    error file clear

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,32 +1,13 @@
-'use client'
+// app/error.tsx
+export const runtime = 'experimental-edge';
 
-
-import React from 'react';
-import { NextPageContext } from 'next';
-
-
-
-
-interface ErrorProps {
-  statusCode: number;
-}
-
-function Error({ statusCode }: ErrorProps) {
-  return (
-    <p>
-      {statusCode
-        ? `An error ${statusCode} occurred on server`
-        : 'An error occurred on client'}
-    </p>
-  );
-}
-
-Error.getInitialProps = ({ res, err }: NextPageContext): ErrorProps => {
-    const statusCode = res?.statusCode ?? err?.statusCode ?? 404;
-    return { statusCode };
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1>Something went wrong!</h1>
+            <p>Please try again later.</p>
+        </div>
+    );
 };
 
-export const runtime = process.env.NEXT_RUNTIME || 'edge';
-
-
-export default Error;
+export default ErrorPage;
