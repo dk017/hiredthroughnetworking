ea11e57b7cdc0f485309c93fea09b6882acd8dad
dk017 <dhineshkumar.stoic@gmail.com>
2024-10-08 18:04:52 +0530

    still error file change since morning

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  };
+  experimental: {
+    runtime: 'edge',
+  },
+  // This ensures that all pages use the Edge Runtime by default
+  reactStrictMode: true,
+  swcMinify: true,
+};
 
-  export default nextConfig;
+export default nextConfig;
